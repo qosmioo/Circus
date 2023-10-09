c4fc18adec693e3ecf57da870c49e578aebabe47
qosmio <sanin.fm1329@gmail.com>
2023-10-09 22:08:55 +0300

    some db models.

---
+++ Circus/Database/Circus.Database.Models/Ticket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Circus.Database.Models;
+
+public class Ticket
+{
+    public Guid Id { get; set; }
+    
+    public Guid SeatId { get; set; }
+    
+    public Guid SessionId { get; set; }
+    
+    public Guid UserId { get; set; }
+    
+    public int Price { get; set; }
+    
+    public bool IsAvailable { get; set; }
+
+    public Ticket(Guid id, 
+        Guid seatId, 
+        Guid sessionId, 
+        Guid userId, 
+        int price, 
+        bool isAvailable)
+    {
+        Id = id;
+        SeatId = seatId;
+        SessionId = sessionId;
+        UserId = userId;
+        Price = price;
+        IsAvailable = isAvailable;
+    }
+}
