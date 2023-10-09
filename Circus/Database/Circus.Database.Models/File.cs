267ce351e198a1a3341010c64b0e95e0bb62d091
qosmio <sanin.fm1329@gmail.com>
2023-10-10 00:09:08 +0300

    added File db model.

---
+++ Circus/Database/Circus.Database.Models/File.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Circus.Database.Models;
+
+public class File
+{
+    public Guid Id { get; set; }
+    
+    public byte[] Data { get; set; }
+    
+    public string Extension { get; set; }
+    
+    public string Name { get; set; }
+
+    public File(Guid id, 
+        byte[] data, 
+        string extension, 
+        string name)
+    {
+        Id = id;
+        Data = data;
+        Extension = extension;
+        Name = name;
+    }
+}
