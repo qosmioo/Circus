59167fb9ffc9a5d54c16500d52d8091c4dc1240c
qosmio <sanin.fm1329@gmail.com>
2023-10-27 23:10:30 +0300

    convertor.

---
+++ Circus/Database/Circus.Database.Repositories/Converters/TicketConverter.cs
@@ -0,0 +1,33 @@
+﻿using DbTicket = Circus.Database.Models.Ticket;
+using CoreTicket = Circus.Core.Models.Ticket;
+
+namespace Circus.Database.Repositories.Converters;
+
+public class TicketConverter
+{
+    public static CoreTicket? ConvertToCore(DbTicket? dbTicket)
+    {
+        if (dbTicket is null)
+            return null;
+
+        return new CoreTicket(dbTicket.Id, 
+            dbTicket.SeatId, 
+            dbTicket.SessionId, 
+            dbTicket.UserId,
+            dbTicket.Price, 
+            dbTicket.IsAvailable);
+    }
+
+    public static DbTicket? ConvertToDb(CoreTicket? coreTicket)
+    {
+        if (coreTicket is null)
+            return null;
+
+        return new DbTicket(coreTicket.Id, 
+            coreTicket.SeatId, 
+            coreTicket.SessionId, 
+            coreTicket.UserId, 
+            coreTicket.Price,
+            coreTicket.IsAvailable);
+    }
+}
