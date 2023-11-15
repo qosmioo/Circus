9372b551396a14cc6837e78d8322a98b9e7a455b
qosmio <sanin.fm1329@gmail.com>
2023-11-15 13:14:03 +0300

    added last controllers.

---
+++ Circus/Circus.Server/Controllers/SessionsController.cs
@@ -0,0 +1,137 @@
+﻿using Circus.Core.Repositories;
+using Circus.Dto.Http;
+using Circus.Server.Controllers.Converters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Circus.Server.Controllers;
+
+[ApiController]
+[Route("api/session")]
+public class SessionsController : ControllerBase
+{
+    private readonly ISessionRepository _sessionRepository;
+
+    private readonly ILogger<SessionsController> _logger;
+
+    private readonly ITicketRepository _ticketRepository;
+
+    public SessionsController(ISessionRepository sessionRepository, ILogger<SessionsController> logger, ITicketRepository ticketRepository)
+    {
+        _sessionRepository = sessionRepository;
+        _logger = logger;
+        _ticketRepository = ticketRepository;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetSessionsAsync()
+    {
+        try
+        {
+            var sessions = await _sessionRepository.GetSessionsAsync();
+            var dtoSessions = sessions.Select(SessionConverter.ConvertSessionToDto).ToList();
+
+            return Ok(dtoSessions);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "Error in method: {MethodName}", nameof(GetSessionsAsync));
+
+            return StatusCode(500);
+        }
+    }
+    
+    [HttpGet]
+    [Route("{sessionId:guid}")]
+    public async Task<IActionResult> GetSessionAsync([FromRoute] Guid sessionId)
+    {
+        try
+        {
+            var session = await _sessionRepository.FindSessionAsync(sessionId);
+
+            if (session == null)
+                return NotFound();
+
+            return Ok(SessionConverter.ConvertSessionToDto(session));
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "Error in method: {MethodName}", nameof(GetSessionAsync));
+
+            return StatusCode(500);
+        }
+    }
+    
+    [HttpPut]
+    public async Task<IActionResult> PutSessionAsync([FromBody] Session session)
+    {
+        try
+        {
+            if (await _sessionRepository.ExistAsync(session.Id))
+            {
+                await _sessionRepository.UpdateSessionAsync(session.Id, 
+                    session.ShowId, 
+                    session.HallId, 
+                    session.StartsAt);
+                
+                foreach (var ticket in session.Tickets)
+                {
+                    await _ticketRepository.UpdateTicketsAsync(session.Id, 
+                        ticket.SessionId,
+                        ticket.SessionId,
+                        ticket.UserId, 
+                        ticket.Price,
+                        ticket.IsAvailable);
+                }
+            }
+            else
+            {
+                await _sessionRepository.AddSessionAsync(session.Id, 
+                    session.ShowId, 
+                    session.HallId, 
+                    session.StartsAt);
+                
+                foreach (var ticket in session.Tickets)
+                {
+                    await _ticketRepository.AddTicketsAsync(session.Id, 
+                        ticket.SessionId,
+                        ticket.SessionId,
+                        ticket.UserId, 
+                        ticket.Price,
+                        ticket.IsAvailable);
+                }
+            }
+            
+            return Ok();
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "Error in method: {MethodName}", nameof(PutSessionAsync));
+
+            return StatusCode(500);
+        }
+    }
+    
+    [HttpDelete]
+    [Route("{sessionId:guid}")]
+    public async Task<IActionResult> DeleteSessionAsync([FromRoute] Guid sessionId)
+    {
+        try
+        {
+            await _sessionRepository.RemoveSessionAsync(sessionId);
+
+            return Ok();
+        }
+        catch (InvalidOperationException exception)
+        {
+            _logger.LogError(exception, "Error in method: {MethodName}", nameof(DeleteSessionAsync));
+
+            return NotFound();
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "Error in method: {MethodName}", nameof(DeleteSessionAsync));
+
+            return StatusCode(500);
+        }
+    }
+}
---
+++ Circus/Database/Circus.Database.Repositories/Converters/FileConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity.Core.Common.CommandTrees;
+using Microsoft.Extensions.FileProviders;
+using CoreFile = Circus.Core.Models.File;
+using DbFile = Circus.Database.Models.File;
+
+namespace Circus.Database.Repositories.Converters;
+
+public static class FileConverter
+{
+    public static CoreFile? ConvertFileToCore(DbFile? file)
+    {
+        return file == null ? null : new CoreFile(file.Id, file.Data, file.Extension, file.Name);
+    }
+
+    public static DbFile? ConvertFileToDb(CoreFile? file)
+    {
+        return file == null ? null : new DbFile(file.Id, file.Data, file.Extension, file.Name);
+    }
+    
+}
